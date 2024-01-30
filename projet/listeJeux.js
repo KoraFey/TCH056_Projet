bd8dd0cf671ba8323b609095b4a0a7ec0dd9e20f
KoraFey <149097374+KoraFey@users.noreply.github.com>
2024-01-30 13:23:16 -0500

    tableaux

--- projet/listeJeux.js
+++ projet/listeJeux.js
@@ -4,79 +4,92 @@ let listeJeux = [
         id:"baba",
         titre:"Baba is you",
         categorie:["indie","strategy"],
-        plateforme:["pc","switch"]
+        plateforme:["pc","switch"],
+        url:["./img/babaisyou.jpg"]
     },
     cod={
         id:"cod",
         titre:"Call of Duty",
         categorie:["shooter","action","massivelyMulti"],
-        plateforme:["pc","xbox","ps"]
+        plateforme:["pc","xbox","ps"],
+        url:["./img/COD.jpg"]
     },
     destiny={
         id:"destiny",
         titre:"Destiny 2",
         categorie:["shooter","massivelyMulti"],
-        plateforme:["pc","xbox","ps"]
+        plateforme:["pc","xbox","ps"],
+        url:["./img/Destiny_2.jpg"]
     },
     lethalC={
         id:"lethalC",
         titre:"Lethal Company",
         categorie:["indie","strategy","horror","action"],
-        plateforme:["pc"]
+        plateforme:["pc"],
+        url:["./img/lethal-company.jfif"]
     },
     puzzleA={
         id:"puzzleA",
         titre:"Puzzle Agent",
         categorie:["casual","action"],
-        plateforme:["pc","ps"]
+        plateforme:["pc","ps"],
+        url:["./img/puzzle_agent.jpg"]
     },
     streetF={
         id:"streetF",
         titre:"Street Fighter",
         categorie:["fighting"],
-        plateforme:["pc","ps"]
+        plateforme:["pc","ps"],
+        url:["./img/Street_Fighter.png"]
     },
     sekiro={
         id:"sekiro",
         titre:"Sekiro",
         categorie:["adventure","rpg"],
-        plateforme:["pc","switch"]
+        plateforme:["pc","switch"],
+        url:["./img/Sekiro.jpg"]
     },
     hzd={
         id:"hzd",
         titre:"Horizon Zero Down",
         categorie:["rpg","adventure"],
-        plateforme:["ps"]
+        plateforme:["ps"],
+        url:["./img/Horizon_Zero_Dawn.jpg"]
     },
     zelda={
         id:"zelda",
         titre:"Zelda",
         categorie:["adventure","strategy","family"],
-        plateforme:["switch"]
+        plateforme:["switch"],
+        url:["./img/ze.jpg"]
     },
     minecraft={
         id:"minecraft",
         titre:"Minecraft",
         categorie:["action","adventure","simulation","family"],
-        plateforme:["pc","switch","xbox","ps"]
+        plateforme:["pc","switch","xbox","ps"],
+        url:["./img/Minecraft.png"]
     },
     pubg={
         id:"pubg",
         titre:"PUBG",
         categorie:["massivelyMulti","shooter"],
-        plateforme:["pc","xbox","ps"]
+        plateforme:["pc","xbox","ps"],
+        url:["./img/pubg-battlegrounds.jfif"]
     },
     mk={
         id:"mk",
         titre:"Mario Kart",
         categorie:["racing","casual","family"],
-        plateforme:["switch"]
+        plateforme:["switch"],
+        url:["./img/mariokart.jpg"]
     },
     burnout3={
         id:"burnout3",
         titre:"Burnout 3",
         categorie:["racing","action"],
-        plateforme:["xbox","ps"]
+        plateforme:["xbox","ps"],
+        url:["./img/burnout3.jpg"]
     }
 ]
 //CATEGORIES
@@ -99,77 +112,99 @@ let listeCategorie=[
     rpg={
         id:"rpg",
         nom:"RPG",
-        url:"./img/action.jpg"
+        url:"./img/rpg.avif"
     },
     strategy={
         id:"strategy",
         nom:"Strategy",
-        url:"./img/action.jpg"
+        url:"./img/echec.jpg"
     },
     shooter={
         id:"shooter",
         nom:"Shooter",
-        url:"./img/action.jpg"
+        url:"./img/shooter.webp"
     },
     casual={
         id:"casual",
         nom:"Casual",
-        url:"./img/action.jpg"
+        url:"./img/casual.png"
     },
     simulation={
         id:"simulation",
         nom:"Simulation",
-        url:"./img/action.jpg"
+        url:"./img/simulation.webp"
     },
     puzzle={
         id:"puzzle",
         nom:"Puzzle",
-        url:"./img/action.jpg"
+        url:"./img/puzzle.jpg"
     },
     arcade={
         id:"arcade",
         nom:"Arcade",
-        url:"./img/action.jpg"
+        url:"./img/arcade.jpg"
     },
     plateformer={
         id:"platformer",
         nom:"Platformer",
-        url:"./img/action.jpg"
+        url:"./img/platformer.jpg"
     },
     massivelyMulti={
         id:"massivelyMulti",
         nom:"Massively Multiplayer",
-        url:"./img/action.jpg"
+        url:"./img/massively.webp"
     },
     racing={
         id:"racing",
         nom:"Racing",
-        url:"./img/action.jpg"
+        url:"./img/racing.jfif"
     },
     sport={
         id:"sport",
         nom:"Sport",
-        url:"./img/action.jpg"
+        url:"./img/sport.png"
     },
     fighting={
         id:"fighting",
         nom:"Fighting",
-        url:"./img/action.jpg"
+        url:"./img/fight.png"
     },
     family={
         id:"family",
         nom:"Family",
-        url:"./img/action.jpg"
+        url:"./img/family.jpg"
     },
     boardGame={
         id:"boardGame",
         nom:"Board Game",
-        url:"./img/action.jpg"
+        url:"./img/boardgames.jpg"
     },
     horror={
         id:"horro",
         nom:"Horror",
-        url:"./img/action.jpg"
+        url:"./img/horror.webp"
     }
 ]
 //PLATFORME
+let listePlatforme =[
+    pc ={
+        id: "pc",
+        nom: "pc",
+        url:["./img/computer.png"]
+    },
+    playstation ={
+        id: "ps",
+        nom: "ps",
+        url:["./img/playstation.png"]
+    },
+    nintendo ={
+        id: "ninswi",
+        nom: "switch",
+        url:["./img/nintendo-switch.png"]
+    },
+    xbox ={
+        id: "xbox",
+        nom: "xbox",
+        url:["./img/xbox-logo.png"]
+    }
+]
